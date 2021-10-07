df2b90bb73f9ec6896aa203970ec659f97cbf740
Mads Hougesen <madshougesen@gmail.com>
2021-10-08 00:36:11 +0200

    refactor: greatly improved time to run program by reducing amount of puppeteer functions

--- src/index.js
+++ src/index.js
@@ -53,88 +53,42 @@ const allData = [];
       break;
     }
 
-    for (let handleNumber = 1; handleNumber < amountPerPage; ++handleNumber) {
-      console.time(`streamer page ${pageNumber} number ${handleNumber}`);
+    const pageData = await page.evaluate(() => {
+      return Array.prototype.slice
+        .call(document.querySelector('#channels > tbody').rows)
+        .map((tr) => Array.prototype.slice.call(tr.cells))
+        .map((td) => {
+          return {
+            handle: td[2]?.outerText?.toLowerCase() || null,
+            average_viewers: td[3]?.outerText || null,
+            total_followers: td[9]?.outerText || null,
+            twitch_link: td[2]?.outerText ? `https://twitch.tv/${td[2]?.outerText?.toLowerCase()}` : null,
+          };
+        })
+        .filter((elm) => elm?.handle !== null);
+    });
 
-      const streamer = {
-        handle: null,
-        language: LANGUAGE || '',
-        average_viewers: null,
-        total_followers: null,
-        twitch_link: null,
-      };
+    allData.push(...pageData);
 
-      // Twitch tracker has tr in their table without data in them, so we need to make sure they exists before setting the data on each eval
-      try {
-        streamer.handle = await page.$eval(
-          `#channels > tbody > tr:nth-child(${handleNumber}) > td:nth-child(3) > a`,
-          (el) => el?.innerText?.toLowerCase(),
-        );
-      } catch (error) {
-        console.log('error getting handle', error);
-        continue;
-      }
-
-      if (!streamer?.handle?.length > 0) {
-        console.log('streamer handle length is 0 OR null');
-      }
-
-      streamer.twitch_link = `https://twitch.tv/${streamer.handle}`;
-
-      try {
-        streamer.average_viewers = await page.$eval(
-          `#channels > tbody > tr:nth-child(${handleNumber}) > td.color-viewers.active > span`,
-          (el) => el.innerText.toLowerCase(),
-        );
-      } catch (error) {
-        console.log('error getting avg viewers', error);
-        continue;
-      }
-
-      try {
-        streamer.total_followers = await page.$eval(
-          `#channels > tbody > tr:nth-child(${handleNumber}) > td:nth-child(10) > span`,
-
-          (el) => el.innerText.toLowerCase(),
-        );
-      } catch (error) {
-        console.log('error getting total followers', error);
-        continue;
-      }
-
-      allData.push(streamer);
-
-      console.timeEnd(`streamer page ${pageNumber} number ${handleNumber}`);
-    }
-
-    console.timeEnd(`page ${pageNumber}`);
+    console.log(allData);
   }
 
-  for (const streamer of allData) {
-    // If total_followers is higher than 1000 TwitchTracker shortens it with a K
-    if (streamer.total_followers.toLowerCase().includes('k')) {
-      streamer.total_followers = parseFloat(streamer.total_followers) * 1000;
-    }
-
-    // TwitchTracker uses commas to seperate values
-    streamer.average_viewers = streamer.average_viewers.replaceAll(',', '');
-  }
+  await browser.close();
 
   // Make sure data folder exists
   if (!fs.existsSync(`./data`)) {
     fs.mkdirSync(`./data`);
   }
 
+  // Make sure language folder exists
   if (!fs.existsSync(`./data/${LANGUAGE || 'all'}`)) {
     fs.mkdirSync(`./data/${LANGUAGE || 'all'}`);
   }
-
-  await browser.close();
 
   // Save data as json
   const csv = new ObjectsToCsv(allData);
   await csv.toDisk(`./data/${LANGUAGE || 'all'}/${LANGUAGE || 'all'}-${new Date().toISOString()}.csv`);
   console.log('Done saving as csv');
 
-  console.time('total');
+  console.timeEnd('total');
 })();
